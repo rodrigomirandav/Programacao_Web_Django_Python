739ce9651a02b238ba80202e6641332323b998ac
Rodrigo de Miranda Videira <diguimirandajf@gmail.com>
2022-02-16 16:56:24 -0300

    Aprendendo sobre banco de dados, buscando produtos, visualizando página simples de produto.

--- django1/core/views.py
+++ django1/core/views.py
@@ -1,13 +1,27 @@
 from django.shortcuts import render
 
+from core.models import Produto
+
 
 def index(request):
+    produtos = Produto.objects.all()
+
     context = {
         'curso': 'Programação web com Django',
-        'outro' : 'Django é massa!',
+        'outro': 'Django é massa!',
+        'produtos': produtos,
     }
     return render(request, 'index.html', context)
 
 
 def contato(request):
     return render(request, 'contato.html')
+
+def produto(request, pk):
+    prod = Produto.objects.get(id=pk)
+
+    context = {
+        'produto': prod
+    }
+
+    return render(request, 'produto.html',context)
