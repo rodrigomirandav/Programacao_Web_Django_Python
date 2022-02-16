eae212dbfc8e6f2b3e08e0d6b04feba485dd65ca
Rodrigo de Miranda Videira <diguimirandajf@gmail.com>
2022-02-16 15:33:34 -0300

    Aprendendo sobre models and migrates

--- django1/core/models.py
+++ django1/core/models.py
@@ -1,4 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Produto(models.Model):
+    nome = models.CharField('Nome', max_length=100)
+    preco = models.DecimalField('Preço', decimal_places=2, max_digits=15)
+    estoque = models.IntegerField('Quantidad em estoque')
 
+class Cliente(models.Model):
+    nome = models.CharField('Nome', max_length=100)
+    sobrenome = models.CharField('Sobrenome', max_length=100)
+    email = models.EmailField('E-mail', max_length=100)
