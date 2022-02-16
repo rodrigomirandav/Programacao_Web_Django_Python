60575f40f32a0237c9bd7f32f9490125124f2aa4
Rodrigo de Miranda Videira <diguimirandajf@gmail.com>
2022-02-16 15:49:39 -0300

    Aprendendo sobre área administrativa Django

--- django1/core/models.py
+++ django1/core/models.py
@@ -3,7 +3,10 @@ from django.db import models
 class Produto(models.Model):
     nome = models.CharField('Nome', max_length=100)
     preco = models.DecimalField('Preço', decimal_places=2, max_digits=15)
-    estoque = models.IntegerField('Quantidad em estoque')
+    estoque = models.IntegerField('Quantidade em estoque')
+
+    def __str__(self):
+        return self.nome
 
 class Cliente(models.Model):
     nome = models.CharField('Nome', max_length=100)
